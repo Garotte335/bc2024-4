131bf29b2cd6b149fd9b1f61c9daeb1bb4f10a23
Svyshch bc2024-4 <den.svyshch@gmail.com>
2024-10-15 13:49:07 +0300

    Додано параметри командного рядка та Веб сервер

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-console.log("Hello, World!");
+const http = require('http');
+const { program } = require('commander');
+
+// Налаштовуємо програму для прийому аргументів командного рядка
+program
+  .requiredOption('-h, --host <host>', 'Server host')
+  .requiredOption('-p, --port <port>', 'Server port')
+  .requiredOption('-c, --cache <path>', 'Path to cache directory')
+  .parse(process.argv);
+
+const options = program.opts();
+
+// Створюємо HTTP сервер
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello from the web server!\n');
+});
+
+// Запускаємо сервер з переданими параметрами host і port
+server.listen(options.port, options.host, () => {
+  console.log(`Server running at http://${options.host}:${options.port}/`);
+});
